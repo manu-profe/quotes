f0971737dcba3b379d4282e916573cfd75d273ab
Santiago <gamersanti19@gmail.com>
2022-08-30 12:23:40 -0500

    Eliminar libro

--- src/main/java/com/acs/quotes/quotes/controller/LibroController.java
+++ src/main/java/com/acs/quotes/quotes/controller/LibroController.java
@@ -11,13 +11,25 @@ import java.util.List;
 
 @RestController
 public class LibroController {
-    private final List<Book> libros = new ArrayList<>();
+    private final List<Book> books = new ArrayList<>();
 
     @GetMapping("/book{title}")
     public String searchBook(String title) {
-        for (Book book : libros) {
+        for (Book book : books) {
             if (book.getTittle().equals(title)) {
                 return "Libro encontrado: " + book.getTittle() + " - Autor: " + book.getAutorId() + " -Published at: " + book.getDatePublished();
+            }
+        }
+        return "Libro no encontrado";
+    }
+
+    //Eliminar libro
+    @PostMapping("/delete/{title}")
+    public String deleteBook(String title) {
+        for (Book book : books) {
+            if (book.getTittle().equals(title)) {
+                books.remove(book);
+                return "Libro eliminado";
             }
         }
         return "Libro no encontrado";

4e7e17cb6ce53943a4a280261ff5df193ad59e60
Felipe Ortiz <felipe.ortiz.bedoya@gmail.com>
2022-08-29 21:26:52 -0500

    update Quote class with new attributes/team2

--- src/main/java/model/Quote.java
+++ src/main/java/model/Quote.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.sql.Timestamp;
+
 public class Quote {
     private String phrase;
+    private Timestamp creationDate;
+    private Timestamp updatedDate;
+    private String keyWord;
+
+    // Check
+    private String author;
+    private String book;
 }

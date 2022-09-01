f0cf8ba86a3a0814141efa997466ac7b5e2b6386
Axl Castro <axlcastro100@gmail.com>
2022-08-31 21:32:59 -0500

    No se que cambio hice

--- src/main/java/com/acs/quotes/quotes/controller/ReaderController.java
+++ src/main/java/com/acs/quotes/quotes/controller/ReaderController.java
@@ -1,5 +1,17 @@
 package com.acs.quotes.quotes.controller;
 
+import com.acs.quotes.quotes.model.Reader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
 public class ReaderController {
+    private final List<Reader> read = new ArrayList<>();
+
+   // @GetMapping("/reader")
+   // @GetMapping("/reader")
+
 
 }

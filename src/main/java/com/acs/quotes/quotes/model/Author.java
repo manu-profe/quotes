75153ffbe7017aa5f90773d146643f495e858e4b
unknown <hhmateus@gmail.com>
2022-08-31 21:41:45 -0500

    Delete and read from author

---
+++ src/main/java/com/acs/quotes/quotes/model/Author.java
@@ -0,0 +1,42 @@
+package com.acs.quotes.quotes.model;
+
+public class Author {
+    private String authorId ;
+    private String firstName ;
+    private String lastName ;
+
+    private String nationality ;
+
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}

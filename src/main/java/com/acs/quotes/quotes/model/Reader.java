79fb562b32cd6971b6568bc9b5add7966d402974
Axl Castro <axlcastro100@gmail.com>
2022-08-31 21:04:03 -0500

    creamos controlador y modelo de reader

---
+++ src/main/java/com/acs/quotes/quotes/model/Reader.java
@@ -0,0 +1,49 @@
+package com.acs.quotes.quotes.model;
+
+public class Reader {
+    private String name;
+    private String lastName;
+    private String gender;
+    private String birth;
+    private String documentId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+}

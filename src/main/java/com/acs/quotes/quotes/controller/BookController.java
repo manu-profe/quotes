9163d4c0fbec785eb6cec7ca00682966f28ddd66
Juan Felipe Mesa Ocampo <jmesa@sistepass.com>
2022-08-31 20:36:21 -0500

    Return BAD_REQUEST when book not exists

--- src/main/java/com/acs/quotes/quotes/controller/BookController.java
+++ src/main/java/com/acs/quotes/quotes/controller/BookController.java
@@ -1,16 +1,12 @@
 package com.acs.quotes.quotes.controller;
 
-
 import com.acs.quotes.quotes.model.Book;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 public class BookController {
@@ -27,20 +23,20 @@ public class BookController {
     }
 
     @PostMapping("/book")
-    public ResponseEntity<Book> saveBook(@RequestBody Book book){
+    public ResponseEntity<Book> saveBook(@RequestBody Book book) {
         for (Book bookEntity : books) {
-            if(bookEntity.getTittle().equals(book.getTittle())){
-                return new ResponseEntity<>(book,null, HttpStatus.BAD_REQUEST);
+            if (bookEntity.getTittle().equals(book.getTittle())) {
+                return new ResponseEntity<>(book, null, HttpStatus.BAD_REQUEST);
             }
         }
         String id = Integer.toString(books.size() + 1);
         book.setId(id);
         books.add(book);
-        return new ResponseEntity<>(book,null, HttpStatus.CREATED);
+        return new ResponseEntity<>(book, null, HttpStatus.CREATED);
     }
 
     @PutMapping("/book")
-    public ResponseEntity<Book> updateBook(@RequestBody Book book){
+    public ResponseEntity<Book> updateBook(@RequestBody Book book) {
         for (Book bookEntity : books) {
             if (bookEntity.getTittle().equals(book.getTittle())) {
                 bookEntity.setId(book.getId());
@@ -48,9 +44,10 @@ public class BookController {
                 bookEntity.setTittle(book.getTittle());
                 bookEntity.setDatePublished(book.getPublishing());
                 bookEntity.setDatePublished(book.getDatePublished());
+                return new ResponseEntity<>(book, null, HttpStatus.OK);
             }
         }
-        return new ResponseEntity<>(book,null, HttpStatus.OK);
+        return new ResponseEntity<>(book, null, HttpStatus.BAD_REQUEST);
     }
 
 }
